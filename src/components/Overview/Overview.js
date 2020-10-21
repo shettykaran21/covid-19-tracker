74c3e64559bb2ea1b935addcb10d061b4fa3e3c3
Karan Shetty <shettykaran2110@gmail.com>
2020-10-21 14:47:30 +0530

    Format number function implemented

--- src/components/Overview/Overview.js
+++ src/components/Overview/Overview.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
 const Overview = ({ totalConfirmed, totalDeaths, totalRecovered }) => {
+  const formatNumber = (num) => {
+    num = num.toString();
+    let afterPoint = '';
+    if (num.indexOf('.') > 0) {
+      afterPoint = num.substring(num.indexOf('.'), num.length);
+    }
+    num = Math.floor(num);
+    num = num.toString();
+    let lastThree = num.substring(num.length - 3);
+    let otherNumbers = num.substring(0, num.length - 3);
+    if (otherNumbers != '') {
+      lastThree = ',' + lastThree;
+    }
+    let res =
+      otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ',') +
+      lastThree +
+      afterPoint;
+
+    return res;
+  };
+
   return (
     <div className="overview">
-      <div>{totalConfirmed}</div>
-      <div>{totalDeaths}</div>
-      <div>{totalRecovered}</div>
+      <div>{formatNumber(totalConfirmed)}</div>
+      <div>{formatNumber(totalDeaths)}</div>
+      <div>{formatNumber(totalRecovered)}</div>
     </div>
   );
 };
