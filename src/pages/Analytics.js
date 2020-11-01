14503e5f9339917429dab9452368e0965233521d
Karan Shetty <shettykaran2110@gmail.com>
2020-11-01 12:01:41 +0530

    Created new page Analytica

---
+++ src/pages/Analytics.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from '../components/Title/Title';
+
+const Analytics = () => {
+  return (
+    <div className="analytics" style={{ marginTop: '8rem' }}>
+      <Title title="Analytics" />
+    </div>
+  );
+};
+
+export default Analytics;
