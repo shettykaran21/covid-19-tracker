2aa126cf8ad1de65072090873c778d90c8dd47ca
Karan Shetty <shettykaran2110@gmail.com>
2020-11-01 17:21:44 +0530

    Made the graph responsive

--- src/components/GlobalGraph/GlobalGraph.js
+++ src/components/GlobalGraph/GlobalGraph.js
@@ -1,6 +1,14 @@
 import React from 'react';
 
-import { LineChart, Line, XAxis, YAxis, Tooltip, Legend } from 'recharts';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from 'recharts';
 
 import Title from '../Title/Title';
 
@@ -15,23 +23,25 @@ const GlobalGraph = ({ dayOneData }) => {
   return (
     <div>
       <Title title="Global Graph" />
-      <LineChart
-        width={1200}
-        height={600}
-        data={dayOneData.data}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
-      >
-        <XAxis tick={false} />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Line type="monotone" dataKey="Cases" stroke="#8884d8" />
-      </LineChart>
+      <ResponsiveContainer width={'100%'} height={400}>
+        <LineChart
+          width={1200}
+          height={600}
+          data={dayOneData.data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <XAxis tick={false} />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="Cases" stroke="#8884d8" />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
