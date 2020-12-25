378c4acbcc4519235ce6a22cac0c462c69395254
Karan Shetty <shettykaran2110@gmail.com>
2020-12-25 12:06:28 +0530

    Fixed newRecovered data

--- src/components/InfoCard/InfoCard.js
+++ src/components/InfoCard/InfoCard.js
@@ -3,28 +3,26 @@ import CountUp from 'react-countup';
 
 import styles from './InfoCard.module.css';
 
+export const formatNumber = (num) => {
+  num = num.toString();
+  let afterPoint = '';
+  if (num.indexOf('.') > 0) {
+    afterPoint = num.substring(num.indexOf('.'), num.length);
+  }
+  num = Math.floor(num);
+  num = num.toString();
+  let lastThree = num.substring(num.length - 3);
+  let otherNumbers = num.substring(0, num.length - 3);
+  if (otherNumbers !== '') {
+    lastThree = ',' + lastThree;
+  }
+  let res =
+    otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ',') + lastThree + afterPoint;
+
+  return res;
+};
+
 const InfoCard = ({ data, title, textColor }) => {
-  const formatNumber = (num) => {
-    num = num.toString();
-    let afterPoint = '';
-    if (num.indexOf('.') > 0) {
-      afterPoint = num.substring(num.indexOf('.'), num.length);
-    }
-    num = Math.floor(num);
-    num = num.toString();
-    let lastThree = num.substring(num.length - 3);
-    let otherNumbers = num.substring(0, num.length - 3);
-    if (otherNumbers !== '') {
-      lastThree = ',' + lastThree;
-    }
-    let res =
-      otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ',') +
-      lastThree +
-      afterPoint;
-
-    return res;
-  };
-
   return (
     <div className={styles.infoCard}>
       <div className={styles.number} style={{ color: textColor }}>
