f6e48392a6468e67f0b17dde9e8906d14fc96011
Karan Shetty <shettykaran2110@gmail.com>
2020-12-25 22:24:46 +0530

    Used custom hook to sort data in table

--- src/components/AnalyticsTable/AnalyticsTable.js
+++ src/components/AnalyticsTable/AnalyticsTable.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState, useMemo } from 'react';
 
 import { formatNumber } from '../InfoCard/InfoCard';
+import useSortableData from '../../hooks/useSortableData';
 
 import styles from './AnalyticsTable.module.css';
 
 const AnalyticsTable = ({ countriesData }) => {
-  const countriesDataRow = countriesData.map((countryData) => {
+  const { items, requestSort, sortConfig } = useSortableData(countriesData);
+
+  const getClassNamesFor = (name) => {
+    if (!sortConfig) {
+      return;
+    }
+    return sortConfig.key === name ? sortConfig.direction : undefined;
+  };
+
+  const countriesDataRow = items.map((item) => {
     return (
-      <tr key={countryData.CountryCode}>
+      <tr key={item.CountryCode}>
         <td>
           <img
-            src={`https://www.countryflags.io/${countryData.CountryCode}/flat/32.png`}
+            src={`https://www.countryflags.io/${item.CountryCode}/flat/32.png`}
           />
-          {countryData.Country}
+          {item.Country}
         </td>
-        <td>{formatNumber(countryData.TotalConfirmed)}</td>
-        <td>{formatNumber(countryData.TotalDeaths)}</td>
-        <td>{formatNumber(countryData.TotalRecovered)}</td>
+        <td>{formatNumber(item.TotalConfirmed)}</td>
+        <td>{formatNumber(item.TotalDeaths)}</td>
+        <td>{formatNumber(item.TotalRecovered)}</td>
       </tr>
     );
   });
@@ -25,10 +35,42 @@ const AnalyticsTable = ({ countriesData }) => {
     <table className={styles.analyticsTable}>
       <thead>
         <tr>
-          <th>Location</th>
-          <th>Confirmed</th>
-          <th>Dead</th>
-          <th>Recovered</th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort('Country')}
+              className={getClassNamesFor('Country')}
+            >
+              Location
+            </button>
+          </th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort('TotalConfirmed')}
+              className={getClassNamesFor('TotalConfirmed')}
+            >
+              Confirmed
+            </button>
+          </th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort('TotalDeaths')}
+              className={getClassNamesFor('TotalDeahs')}
+            >
+              Deaths
+            </button>
+          </th>
+          <th>
+            <button
+              type="button"
+              onClick={() => requestSort('TotalRecovered')}
+              className={getClassNamesFor('TotalRecovered')}
+            >
+              Recovered
+            </button>
+          </th>
         </tr>
       </thead>
       <tbody>{countriesDataRow}</tbody>
