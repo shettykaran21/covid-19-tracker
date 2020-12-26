911af49efe089aec98aa2c6c5f716010123ddbbf
Karan Shetty <shettykaran2110@gmail.com>
2020-12-26 15:36:06 +0530

    Removed container class

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,7 +11,7 @@ import styles from './Navbar.module.css';
 const Navbar = () => {
   return (
     <nav className={`${styles.navbar}`}>
-      <div className={`${styles.container} container`}>
+      <div className={`${styles.container}`}>
         <div className={styles.logo}>
           <Link to="/">
             <img src={logo} alt="logo" className={styles.logo} />
