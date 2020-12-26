3beef9b4b59fb7b4948b08a602d7a5df03893363
Karan Shetty <shettykaran2110@gmail.com>
2020-12-26 21:22:03 +0530

    Formatting

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ import Home from './pages/Home';
 import Map from './pages/Map';
 import Analytics from './pages/Analytics';
 import AnalyticsCountry from './pages/AnalyticsCountry';
+import AboutCovid19 from './pages/AboutCovid19';
+import FlattenTheCurve from './pages/FlattenTheCurve';
 
 import SummaryContextProvider from './contexts/SummaryContext';
 import DayOneContextProvider from './contexts/DayOneContext';
 
 import './App.css';
-import AboutCovid19 from './pages/AboutCovid19';
-import FlattenTheCurve from './pages/FlattenTheCurve';
 
 const App = () => {
   return (
