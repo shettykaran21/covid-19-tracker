e7675bb4f0fd69bad02c03791626356cfe40dfc4
Karan Shetty <shettykaran2110@gmail.com>
2020-12-27 19:29:35 +0530

    Conditional render in Map

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -6,35 +6,41 @@ import { SummaryContext } from '../contexts/SummaryContext';
 
 const Map = () => {
   const [content, setContent] = useState({});
-
   const { countriesData } = useContext(SummaryContext);
 
   return (
     <div style={{ marginTop: 'var(--nav-height)', padding: '' }}>
       {/* <Title title="Covid-19 Map" /> */}
-      <MapChart setTooltipContent={setContent} countriesData={countriesData} />
-      <ReactTooltip>
-        <div style={{ fontSize: '1.6rem', textAlign: 'center' }}>
-          <h2>{content.NAME}</h2>
-          <div>
-            {content.TotalConfirmed
-              ? `Total Confirmed: ${content.TotalConfirmed}`
-              : ''}{' '}
-          </div>
-          <div>
-            {' '}
-            {content.TotalDeaths
-              ? `Total Deaths: ${content.TotalDeaths}`
-              : ''}{' '}
-          </div>
-          <div>
-            {' '}
-            {content.TotalRecovered
-              ? `Total Recovered: ${content.TotalRecovered}`
-              : ''}{' '}
-          </div>
+      {countriesData ? (
+        <div>
+          <MapChart
+            setTooltipContent={setContent}
+            countriesData={countriesData}
+          />
+          <ReactTooltip>
+            <div style={{ fontSize: '1.6rem', textAlign: 'center' }}>
+              <h2>{content.NAME}</h2>
+              <div>
+                {content.TotalConfirmed
+                  ? `Total Confirmed: ${content.TotalConfirmed}`
+                  : ''}{' '}
+              </div>
+              <div>
+                {' '}
+                {content.TotalDeaths
+                  ? `Total Deaths: ${content.TotalDeaths}`
+                  : ''}{' '}
+              </div>
+              <div>
+                {' '}
+                {content.TotalRecovered
+                  ? `Total Recovered: ${content.TotalRecovered}`
+                  : ''}{' '}
+              </div>
+            </div>
+          </ReactTooltip>
         </div>
-      </ReactTooltip>
+      ) : null}
     </div>
   );
 };
