3543f3b2fcedf9ae45ba8ed4c7382ed9414b4d1d
Karan Shetty <shettykaran2110@gmail.com>
2020-12-28 15:14:37 +0530

    HTTPS request instead of HTTP

--- src/api/news.js
+++ src/api/news.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const API_KEY = process.env.REACT_APP_NEWS_API_KEY;
 
 export default axios.create({
-  baseURL: 'http://newsapi.org/v2',
+  baseURL: 'https://newsapi.org/v2',
   params: {
     apiKey: API_KEY,
   },
